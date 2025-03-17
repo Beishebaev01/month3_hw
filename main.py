666d17ebed39d3e13e4a11b7640dbc915cb8cc34
Beishebaev01 <bshv228@icloud.com>
2025-03-18 02:17:50 +0600

    adding home work 2-3

---
+++ main.py
@@ -0,0 +1,71 @@
+import flet as ft
+import datetime as dt
+
+def main(page: ft.Page):
+    page.title = "First Desktop App"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    
+    greeting_text = ft.Text("Hello, World!")
+
+    greeting_history = []
+
+    history_text = ft.Text("History", style="bodyMedium")
+
+
+    def on_button_click(_):
+        name = name_input.value.strip()
+        if name:
+            greeting_text.value = f"Hello, {name}!"
+            greet_button.text = "Hi again"
+            name_input.value = ""
+            time = dt.datetime.now().strftime("%H:%M:%S")
+            if time > "06:00:00" and time < "12:00:00":
+                greeting_text.value = f"Good morning, {name}!"
+            elif time > "12:00:00" and time < "18:00:00":
+                greeting_text.value = f"Good afternoon, {name}!"
+            elif time > "18:00:00" and time < "23:59:59":
+                greeting_text.value = f"Good evening, {name}!"
+            else:
+                greeting_text.value = f"Good night, {name}!"
+            greeting_history.append(f"{time} - {name}")
+            history_text.value = "History: " + "\n".join(greeting_history)
+        else:
+            greeting_text.text = "Enter your name"
+
+        page.update()
+
+
+    name_input = ft.TextField(label="Enter your name:", autofocus=True, on_submit=on_button_click)
+
+
+    def clear_history(_):
+        greeting_history.clear()
+        history_text.value = "History"
+        page.update()
+
+
+    def toggle_theme(_):
+        if page.theme_mode == ft.ThemeMode.LIGHT:
+            page.theme_mode = ft.ThemeMode.DARK
+        else:
+            page.theme_mode = ft.ThemeMode.LIGHT
+
+        page.update()
+
+    
+    theme_button = ft.IconButton(icon=ft.icons.BRIGHTNESS_6, tooltip="Change mode", on_click=toggle_theme)
+
+    greet_button = ft.ElevatedButton("Hi", on_click=on_button_click)
+
+    clear_button = ft.IconButton(icon=ft.icons.DELETE, tooltip="Clear", on_click=clear_history)
+
+    page.add(ft.Row([theme_button], alignment=ft.MainAxisAlignment.END), 
+             greeting_text, 
+             name_input, 
+             greet_button,
+             clear_button,
+             history_text
+             )
+    
+
+ft.app(target=main, view=ft.WEB_BROWSER)
